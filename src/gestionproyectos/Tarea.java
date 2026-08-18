1ad6465c3e39ab051091f6c78d5dda5a3eda3e20
MrGato-sudo <sescarragar@udistrital.edu.co>
2026-08-17 20:11:10 -0500

    feat: agrega constructor de Tarea

---
+++ src/gestionproyectos/Tarea.java
@@ -0,0 +1,10 @@
+java
+public class Tarea {
+    private String nombre;
+    private boolean completada;
+
+    public Tarea(String nombre) {
+        this.nombre = nombre;
+        this.completada = false;
+    }
+}
